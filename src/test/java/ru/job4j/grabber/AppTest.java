c551a4ce410ae407eceac5435beb6ca8d6d34dd2
serj <sergeykalenikov@gmail.com>
2021-05-18 19:40:08 +0300

    add test

---
+++ src/test/java/ru/job4j/grabber/AppTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.grabber;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void thenTest(){
+        assertTrue(true);
+    }
+
+}
