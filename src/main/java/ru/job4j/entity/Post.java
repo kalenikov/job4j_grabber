ab7c23188af73f350a89446ff7783d708264939e
serj <sergeykalenikov@gmail.com>
2021-05-24 17:02:50 +0300

    add getId

--- src/main/java/ru/job4j/entity/Post.java
+++ src/main/java/ru/job4j/entity/Post.java
@@ -9,11 +9,33 @@ public class Post {
     private String link;
     private LocalDateTime created;
 
-    public Post(String name, String text, String link, LocalDateTime created) {
+    public Post(int id, String name, String text, String link, LocalDateTime created) {
+        this.id = id;
         this.name = name;
         this.text = text;
         this.link = link;
         this.created = created;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     @Override
