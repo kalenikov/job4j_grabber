37579b61bbf7d30d32d830f90e421842e5e012f1
serj <sergeykalenikov@gmail.com>
2021-06-09 20:15:47 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+        String rsl = new GeneratorImpl().produce("var: ${k1}",
+                Map.of("k1", "v"));
+        assertThat(rsl, is("var: v"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenKeyNotFound() {
+        new GeneratorImpl().produce("var: ${k2}",
+                Map.of("k1", "v"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenExtraKeys() {
+        new GeneratorImpl().produce("var: ${k1}",
+                Map.of("k1", "v", "k2", "v"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenIllegalTemplate() {
+        new GeneratorImpl().produce("var: ${k1 k2}",
+                Map.of("k1", "v", "k2", "v"));
+    }
+}
